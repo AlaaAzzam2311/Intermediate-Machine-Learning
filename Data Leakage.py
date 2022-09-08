d31616e074283522ec7c53757415867fa7b88a3a
AlaaAzzam2311 <105333232+AlaaAzzam2311@users.noreply.github.com>
2022-09-08 08:26:21 +0200

    Data Leakage.py

---
+++ Data Leakage.py
@@ -0,0 +1,6 @@
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_intermediate.ex7 import *
+print("Setup Complete")
+potential_leakage_feature = 2
